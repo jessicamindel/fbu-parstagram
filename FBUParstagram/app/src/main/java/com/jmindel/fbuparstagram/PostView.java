60a9fb319a84cac7b927cefa47414c2f4b273a27
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-09 17:30:05 -0700

    Add PostView, refactor TimelineFragment for better encapsulation

---
+++ FBUParstagram/app/src/main/java/com/jmindel/fbuparstagram/PostView.java
@@ -0,0 +1,84 @@
+package com.jmindel.fbuparstagram;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.jmindel.fbuparstagram.adapters.PostAdapter;
+import com.jmindel.fbuparstagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostView extends RecyclerView {
+    private PostAdapter adapter;
+    private List<Post> posts;
+    private Handler handler;
+
+    public PostView(@NonNull Context context) {
+        super(context);
+    }
+
+    public PostView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        posts = new ArrayList<>();
+        adapter = new PostAdapter((Activity) getContext(), posts);
+        setAdapter(adapter);
+        setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    public PostView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        posts = new ArrayList<>();
+        adapter = new PostAdapter((Activity) getContext(), posts);
+        setAdapter(adapter);
+        setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
+    public Post getPost(int i) {
+        return this.posts.get(i);
+    }
+
+    public void runQuery() {
+        final Post.Query postsQuery = handler.makeQuery();
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("PostView", "Post[" + i + "] = "
+                                + objects.get(i).getCaption()
+                                + " : username = " + objects.get(i).getUser().getUsername()
+                        );
+                    }
+                    // TODO: Update, not just override
+                    posts.clear();
+                    posts.addAll(objects);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static class Handler {
+        public Post.Query makeQuery() {
+            Post.Query postsQuery = new Post.Query();
+            postsQuery.getTop().withUser();
+            postsQuery.orderByDescending(Post.KEY_CREATED_AT);
+            return postsQuery;
+        }
+    }
+}
--- FBUParstagram/app/src/main/java/com/jmindel/fbuparstagram/fragments/TimelineFragment.java
+++ FBUParstagram/app/src/main/java/com/jmindel/fbuparstagram/fragments/TimelineFragment.java
@@ -5,36 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.jmindel.fbuparstagram.PostView;
 import com.jmindel.fbuparstagram.R;
 import com.jmindel.fbuparstagram.activities.LoginActivity;
-import com.jmindel.fbuparstagram.adapters.PostAdapter;
-import com.jmindel.fbuparstagram.model.Post;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TimelineFragment extends Fragment {
 
-    PostAdapter adapter;
-    List<Post> posts;
-
-    @BindView(R.id.rvPosts) RecyclerView rvPosts;
+    @BindView(R.id.pvPosts) PostView pvPosts;
 
     @Nullable
     @Override
@@ -47,56 +31,31 @@ public class TimelineFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
-        posts = new ArrayList<>();
-        adapter = new PostAdapter((Activity) getContext(), posts);
-        rvPosts.setAdapter(adapter);
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        final Post.Query postsQuery = new Post.Query();
-        postsQuery.getTop().withUser();
-        postsQuery.orderByDescending(Post.KEY_CREATED_AT);
-        postsQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if (e == null) {
-                    for (int i = 0; i < objects.size(); i++) {
-                        Log.d("Home -> Timeline", "Post[" + i + "] = "
-                                + objects.get(i).getCaption()
-                                + " : username = " + objects.get(i).getUser().getUsername()
-                        );
-                    }
-                    // TODO: Update, not just override
-                    posts.clear();
-                    posts.addAll(objects);
-                    adapter.notifyDataSetChanged();
-                } else {
-                    e.printStackTrace();
-                }
-            }
-        });
+        pvPosts.setHandler(new PostView.Handler());
+        pvPosts.runQuery();
     }
 
     // File file = new File(path); --> ParseFile parseFile = new ParseFile(file);
-    private void createPost(String caption, ParseFile image, ParseUser user) {
-        final Post post = new Post();
-        post.setCaption(caption);
-        post.setImage(image);
-        post.setUser(user);
-
-        post.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e != null) {
-                    Log.e("HomeActivity", "Posting failed");
-                    e.printStackTrace();
-                    Toast.makeText(getContext(), "Posting failed", Toast.LENGTH_LONG).show();
-                } else {
-                    Log.d("HomeActivity", "Create post success!");
-                    // TODO: Add to adapter
-                }
-            }
-        });
-    }
+//    private void createPost(String caption, ParseFile image, ParseUser user) {
+//        final Post post = new Post();
+//        post.setCaption(caption);
+//        post.setImage(image);
+//        post.setUser(user);
+//
+//        post.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e != null) {
+//                    Log.e("HomeActivity", "Posting failed");
+//                    e.printStackTrace();
+//                    Toast.makeText(getContext(), "Posting failed", Toast.LENGTH_LONG).show();
+//                } else {
+//                    Log.d("HomeActivity", "Create post success!");
+//                    // TODO: Add to adapter
+//                }
+//            }
+//        });
+//    }
 
     public void logOut(View view) {
         ((Activity) getContext()).getIntent().putExtra(LoginActivity.KEY_LOGGED_OUT, true);
